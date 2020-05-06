1880f32db1903319a9b74739ebb9667f8e531ae8
Uriah Harston <uharston@gmail.com>
2020-05-06 20:11:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,3 @@
-# def my_each(array)
-#   array.each { |i| puts i}
-# end
-#
-# def my_each(array)
-#   i = 0
-#   while i < array.size
-#     i += 1
-#     puts "#{i} love you"
-#   end
-#   puts "We are done here"
-# end
-
-# my_each(collection)
 
 def my_each(array)
   i = 0
