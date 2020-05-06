cb568aa1b486bbc2882300c23cb911a2964debf2
Uriah Harston <uharston@gmail.com>
2020-05-06 19:45:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each(array)
+#   array.each { |i| puts i}
+# end
+# 
+# def my_each(array)
+#   i = 0
+#   while i < array.size
+#     i += 1
+#     puts "#{i} love you"
+#   end
+#   puts "We are done here"
+# end
+
+def my_each(array)
+  i = 0
+  while i < array.size
+    yield array[i]
+    i += 1
+  end
+end
+
+# my_each(collection)
+
+my_each(collection) do |n|
+  puts n
 end
